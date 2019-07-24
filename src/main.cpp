79eee9dab37a8ecaa6fad2171235b838bde01b9c
AndrewRademacher <andrewrademacher@icloud.com>
2019-07-24 00:32:30 -0500

    Fixed builds on mac.

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,15 @@
 #include "ImageWindow.h"
 #include "DrawWindow.h"
 #include "LoginWindow.h"
+#include "calc/CalculatorView.h"
 
 int main(int argc, char* argv[]) {
   Gtk::Main kit(argc, argv);
 //  calc::CalculatorView app;
-//  custom::LoginWindow app;
+  custom::LoginWindow app;
 //  custom::DrawWindow app;
   
-  custom::ImageWindow app;
+//  custom::ImageWindow app;
   Gtk::Main::run(app);
   return EXIT_SUCCESS;
 }
